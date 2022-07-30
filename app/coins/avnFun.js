724ba06e9b15b375e7477ce0f3013be6a992abc1
alamshafil <alamshafil@pm.me>
2022-07-30 16:50:15 -0400

    Add known AVN address info

--- app/coins/avnFun.js
+++ app/coins/avnFun.js
@@ -9,6 +9,42 @@ module.exports = {
 			summary: "The Avian Genesis Block.",
 			alertBodyHtml: "This is the first block in the Avian blockchain, known as the <b>Genesis Block</b>.",
 			referenceUrl: "https://en.bitcoin.it/wiki/Genesis_block"
-		}
+		},
+		{
+			type:"address",
+			date:"2021-08-27",
+			chain: "main",
+			address:"RFdvps1Boq6SngTV3STnCE9DKvnQLTSGMX",
+			summary:"Avian Foundation address",
+			alertBodyHtml:"Avian Foundation address",
+			referenceUrl: "https://avn.network"
+		},
+		{
+			type:"address",
+			date:"2021-08-27",
+			chain: "main",
+			address:"RFdvps1Boq6SngTV3STnCE9DKvnQLTSGMX",
+			summary:"Avian Donation address",
+			alertBodyHtml:"Avian Donation address",
+			referenceUrl: "https://avn.network"
+		},
+		{
+			type:"address",
+			date:"2021-08-27",
+			chain: "main",
+			address:"RGqVXiGTK59MEfyL19PMPiHT4532TvCDY9",
+			summary:"Wrapped Avian address",
+			alertBodyHtml:"Wrapped Avian proof-of-store address",
+			referenceUrl: "https://avn.network"
+		},
+		{
+			type:"address",
+			date:"2021-08-27",
+			chain: "main",
+			address:"RXBurnXXXXXXXXXXXXXXXXXXXXXXWUo9FV",
+			summary:"Burn address",
+			alertBodyHtml:"This is a burn address.",
+			referenceUrl: "https://avn.network"
+		},
 	]
 };
